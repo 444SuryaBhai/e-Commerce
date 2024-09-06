223b57ebf0e4448dca72823e7be96db74d4b2c72
444SuryaBhai <152987429+444SuryaBhai@users.noreply.github.com>
2024-09-06 19:48:30 +0530

    changed

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ app.use('/images',express.static('upload/images'))
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         success:1,
-        image_url:`https://smart-shopper-frontend.onrender.com/images/${req.file.filename}`
+        image_url:`https://smart-shopper-e79a.onrender.com/images/${req.file.filename}`
     })
 })
 
